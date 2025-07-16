9ae0b6adac5bdc5e64a904f5bac6e1e158218df5
Codenames-Ren <renmizuki38@gmail.com>
2025-07-16 14:12:36 +0700

    mengubah warna informasi

--- ui/components/EmptyState.tsx
+++ ui/components/EmptyState.tsx
@@ -1,6 +1,6 @@
 export default function EmptyState() {
   return (
-    <div className="text-center text-muted-foreground mt-10">
+    <div className="text-center mt-3 text-white">
       Masukkan URL video dari <strong>YouTube</strong>, <strong>TikTok</strong>,{" "}
       <strong>Instagram</strong>, atau <strong>Facebook</strong> untuk memulai.
     </div>
